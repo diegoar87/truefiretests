89f519a5f05054e4f1b62f960d94b42156fb62d1
drojas87-photo <diegorojas87@protonmail.com>
2025-08-12 17:13:42 -0300

    feat: added README and basic instructions to tests

--- e2e/findCourse.spec.js
+++ e2e/findCourse.spec.js
@@ -1,20 +1,40 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Find your course spec', () => {
+/* I'd probably fail the tests because of the missing search button component. However in order to create a valuable coding example I'd
+skip that and proceed to add more instructions and code to my tests */
+
+
+const genre = "Metal";
+const instrument = "Guitar";
+const searchNotFound = "searchWithNoResultsText";
+
+test.describe('Find your course functionality', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/browse');
   });
 
-  test('Search by genre ', async ({ page }) => {
+
+test.describe('Search Bar' , () => {  
+  test('should search by genre @regression ', async ({ page }) => {
+    await search(genre);
    
   });
 
-  test('Search by instrument' , async ({page}) => {
+  test('should search by instrument' , async ({page}) => {
+    await search(instrument)
+  });
 
-  })
+  test('should search with no results found' , async ({page}) => {
+    await search(searchNotFound)
+  });
 
-  test('Search by style' , async ({page}) => {
+  test('should find recommended courses on search section' , async ({page}) => {
     
-  })
-  
+  });
+})
+
+function search(text) {
+
+}
+
 });
